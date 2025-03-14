5aaf88371bb478b63e05ec8482e7610a31b2dc16
Ruba <s12028675@stu.najah.edu>
2025-03-14 23:22:51 +0200

    Implement Feature 4

--- StringCalculator/Program.fs
+++ StringCalculator/Program.fs
@@ -1,1 +1,32 @@
-﻿printfn "Hello F#"
+﻿open System
+
+let Add input =
+    match input with
+    | "" -> 0
+    | _ -> 
+        let delimiter, numbers =
+            if input.StartsWith("//") then
+                let line = input.IndexOf("\\n")
+                let delimiter = input.Substring(2,line - 2)
+                let numbers = input.Substring(5)
+                delimiter, numbers
+            else
+                ",", input  
+
+        numbers.Split([| ","; "\\n"; delimiter |], StringSplitOptions.None)
+        |> Array.map int
+        |> Array.sum
+
+
+  
+
+
+printfn "Enter your string input"
+
+let input = Console.ReadLine()
+ 
+try
+    let sum = Add input
+    printfn $"The sum = {sum}"
+with
+    exp -> printfn $"Error: {exp.Message}" 
