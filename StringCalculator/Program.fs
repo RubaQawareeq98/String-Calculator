01bd22f562129a00c197f86579dbfef5b5b11605
Ruba <s12028675@stu.najah.edu>
2025-03-14 22:21:42 +0200

    Implement Feature 2

--- StringCalculator/Program.fs
+++ StringCalculator/Program.fs
@@ -1,1 +1,18 @@
-﻿printfn "Hello F#"
+﻿open System
+
+printfn "Enter your string input"
+
+let input = Console.ReadLine()
+
+let Add input =
+        match input with
+        | "" -> 0
+        | _ -> input.Split(',') 
+                |> Array.map int
+                |> Array.sum
+ 
+try
+    let sum = Add input
+    printfn $"The sum = {sum}"
+with
+    exp -> printfn $"Error: {exp.Message}" 
